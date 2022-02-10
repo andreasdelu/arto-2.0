4a2a14035c2a6136da670c4724fca85332a1bdc2
andreasdelu <Andreasdelu@gmail.com>
2022-02-10 12:27:13 +0100

    done

--- logging.js
+++ logging.js
@@ -1,5 +1,6 @@
 const { json } = require("express/lib/response");
 const fs = require("fs");
+const { dirname } = require("path");
 
 exports.log = function(message){ addToLog(message)};
 
@@ -12,15 +13,49 @@ const day = date.getDate();
 const month = date.getMonth();
 const year = date.getFullYear();
 
-fs.readFile('chatlogs/log' + day + month + year + '.json', function(err, data) {
-    if (err) {
-        throw err;
-    }
-    let chats = JSON.parse(data);
-    chats.forEach(chat => {
-        chatLog.push(chat);
-    });
-});
+const logFile = 'log' + day + month + year + '.json';
+const path = 'chatlogs/';
+
+fs.readdir(__dirname + "/chatlogs", (err, files) => {
+    if (err)
+        console.log(err);
+    else {
+        if (files.length == 0) {
+            console.log("No logs! Creating Log")
+            fs.writeFile(path+logFile, "[]", (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+        }
+        else{
+            files.forEach(file => {
+            if (file == logFile) {
+                console.log("Reading Log")
+                fs.readFile(path+logFile, function(err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    const chats = JSON.parse(data);
+                    chats.forEach(chat => {
+                        chatLog.push(chat);
+                    });
+                });
+            }
+            else{
+                console.log("Creating Log")
+                fs.writeFile(path+logFile, "[]", (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+            
+            })
+        }
+  }
+})
+
 
 function addToLog(message) {
     if (chatLog.length <= 200) {
@@ -37,13 +72,13 @@ function addToLog(message) {
 function writeChatLog() {
     let data = JSON.stringify(chatLog);
     let remove = '';
-    fs.writeFile('chatlogs/log' + day + month + year + '.json', remove, (err) => {
+    fs.writeFile(path+logFile, remove, (err) => {
         if (err) {
             throw err;
         }
     });
 
-    fs.appendFile('chatlogs/log' + day + month + year + '.json', data, (err) => {
+    fs.appendFile(path+logFile, data, (err) => {
         if (err) {
             throw err;
         }
